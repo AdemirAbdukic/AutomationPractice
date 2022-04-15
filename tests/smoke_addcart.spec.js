2b08d0977a17719e2ab89cdc3ea98a5c37849f81
AdemirAbdukic <ademirabdukic@outlook.com>
2022-04-15 14:50:26 +0200

    Fix add to cart

--- tests/smoke_addcart.spec.js
+++ tests/smoke_addcart.spec.js
@@ -2,10 +2,11 @@ const { test, expect } = require('@playwright/test');
 
 test('Add to cart', async ({ page }) => {
   await page.goto('');
+  await page.pause();
   await page.locator('#search_query_top').fill("Faded Short Sleeve T-shirts");
   await page.locator('//button[@class="btn btn-default button-search"]').click();
   await expect(page).toHaveURL('http://automationpractice.com/index.php?controller=search&orderby=position&orderway=desc&search_query=Faded+Short+Sleeve+T-shirts&submit_search=');
-  await page.locator('//a[@class="product_img_link" and @title="Faded Short Sleeve T-shirts"]').click();
+  await page.locator('//h5/a[@title="Faded Short Sleeve T-shirts"]').click();
   await expect(page).toHaveURL('http://automationpractice.com/index.php?id_product=1&controller=product&search_query=Faded+Short+Sleeve+T-shirts&results=1');
   await page.locator('//button[@class="exclusive"]').click();
   await page.locator('//span[@class="cross"]').click();
